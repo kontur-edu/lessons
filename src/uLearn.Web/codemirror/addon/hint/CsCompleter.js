11265d6adb271100241d7734c0a4f68665e61bad
Zaynullin Bulat <zaynullin_bulat@skbkontur.ru>
2014-11-24 11:54:11 +0500

    small fixes if completer generation

--- src/uLearn.Web/codemirror/addon/hint/CsCompleter.js
+++ src/uLearn.Web/codemirror/addon/hint/CsCompleter.js
@@ -5,7 +5,7 @@
 	+ " Add Contains ContainsKey Directory FullName Extension ToLower"
 	+ " Where Select SelectMany ToArray ToList ToDictionary ToLookup Join Zip Aggregate GroupBy OrderBy OrderByDescending ThenBy ThenByDescending ").split(" ");
 
-	this.types = ['int', 'string', 'double', 'Console', 'Math', 'long', 'Boolean', 'double', 'Enumerable', 'Array', 'Enumerable'];
+	this.types = ['int', 'string', 'double', 'Console', 'Math', 'long', 'Boolean', 'double', 'Enumerable', 'Array', 'List'];
 
 	this.dictWithStaticMethods = [];
 	this.dictWithStaticMethods['int'] = ['Parse', 'TryParse'];
@@ -31,14 +31,14 @@
 	this.dictWithNonStaticMethods['double'] = ['CompareTo', 'Equals', 'GetHashCode', 'ToString', 'GetTypeCode', 'GetType'];
 	this.dictWithNonStaticMethods['Enumerable'] = ['ToString', 'Equals', 'GetHashCode', 'GetType'];
 	this.dictWithNonStaticMethods['Array'] = ['GetValue', 'SetValue', 'GetLength', 'GetLongLength', 'GetUpperBound', 'GetLowerBound', 'Clone', 'CopyTo', 'GetEnumerator', 'Initialize', 'ToString', 'Equals', 'GetHashCode', 'GetType'];
-	this.dictWithNonStaticMethods['Enumerable'] = ['Add', 'AddRange', 'AsReadOnly', 'BinarySearch', 'Clear', 'Contains', 'ConvertAll', 'CopyTo', 'Find', 'FindAll', 'FindIndex', 'ForEach', 'GetEnumerator', 'GetRange', 'IndexOf', 'Insert', 'InsertRange', 'Remove', 'RemoveAll', 'RemoveAt', 'RemoveRange', 'Reverse', 'Sort', 'ToArray', 'TrimExcess', 'Exists', 'FindLast', 'FindLastIndex', 'LastIndexOf', 'TrueForAll', 'ToString', 'Equals', 'GetHashCode', 'GetType'];
+	this.dictWithNonStaticMethods['List'] = ['Add', 'AddRange', 'AsReadOnly', 'BinarySearch', 'Clear', 'Contains', 'ConvertAll', 'CopyTo', 'Find', 'FindAll', 'FindIndex', 'ForEach', 'GetEnumerator', 'GetRange', 'IndexOf', 'Insert', 'InsertRange', 'Remove', 'RemoveAll', 'RemoveAt', 'RemoveRange', 'Reverse', 'Sort', 'ToArray', 'TrimExcess', 'Exists', 'FindLast', 'FindLastIndex', 'LastIndexOf', 'TrueForAll', 'ToString', 'Equals', 'GetHashCode', 'GetType'];
 
 
 	this.dictWithProperties = [];
 	this.dictWithProperties['string'] = ['Chars', 'Length'];
 	this.dictWithProperties['Console'] = ['IsInputRedirected', 'IsOutputRedirected', 'IsErrorRedirected', 'In', 'Out', 'Error', 'InputEncoding', 'OutputEncoding', 'BackgroundColor', 'ForegroundColor', 'BufferHeight', 'BufferWidth', 'WindowHeight', 'WindowWidth', 'LargestWindowWidth', 'LargestWindowHeight', 'WindowLeft', 'WindowTop', 'CursorLeft', 'CursorTop', 'CursorSize', 'CursorVisible', 'Title', 'KeyAvailable', 'NumberLock', 'CapsLock', 'TreatControlCAsInput'];
 	this.dictWithProperties['Array'] = ['Length', 'LongLength', 'Rank', 'SyncRoot', 'IsReadOnly', 'IsFixedSize', 'IsSynchronized'];
-	this.dictWithProperties['Enumerable'] = ['Capacity', 'Count', 'Item'];
+	this.dictWithProperties['List'] = ['Capacity', 'Count', 'Item'];
 
 
 	this.dictWithConstants = [];
@@ -52,19 +52,21 @@
 
 
 	this.returnTypeDict = [];
-	this.returnTypeDict['int'] = ['Parse', 'Compare', 'CompareOrdinal', 'Read', 'Ceiling', 'Floor', 'Round', 'Truncate', 'Abs', 'Max', 'Min', 'Sign', 'DivRem', 'Sum', 'Average', 'Count', 'BinarySearch', 'FindIndex', 'FindLastIndex', 'IndexOf', 'LastIndexOf', 'CompareTo', 'GetHashCode', 'IndexOfAny', 'LastIndexOfAny', 'GetLength', 'GetUpperBound', 'GetLowerBound', 'RemoveAll', 'Length', 'BufferHeight', 'BufferWidth', 'WindowHeight', 'WindowWidth', 'LargestWindowWidth', 'LargestWindowHeight', 'WindowLeft', 'WindowTop', 'CursorLeft', 'CursorTop', 'CursorSize', 'Rank', 'Capacity', 'MaxValue', 'MinValue'];
+	this.returnTypeDict['int'] = ['Parse', 'Compare', 'CompareOrdinal', 'Read', 'Abs', 'Max', 'Min', 'Sign', 'DivRem', 'Count', 'Sum', 'BinarySearch', 'FindIndex', 'FindLastIndex', 'IndexOf', 'LastIndexOf', 'CompareTo', 'GetHashCode', 'IndexOfAny', 'LastIndexOfAny', 'GetLength', 'GetUpperBound', 'GetLowerBound', 'RemoveAll', 'Length', 'BufferHeight', 'BufferWidth', 'WindowHeight', 'WindowWidth', 'LargestWindowWidth', 'LargestWindowHeight', 'WindowLeft', 'WindowTop', 'CursorLeft', 'CursorTop', 'CursorSize', 'Rank', 'Capacity', 'MaxValue', 'MinValue'];
 	this.returnTypeDict['Boolean'] = ['TryParse', 'Equals', 'IsNullOrEmpty', 'IsNullOrWhiteSpace', 'IsInfinity', 'IsPositiveInfinity', 'IsNegativeInfinity', 'IsNaN', 'Parse', 'SequenceEqual', 'Any', 'All', 'Contains', 'Exists', 'TrueForAll', 'IsNormalized', 'EndsWith', 'StartsWith', 'Remove', 'IsInputRedirected', 'IsOutputRedirected', 'IsErrorRedirected', 'CursorVisible', 'KeyAvailable', 'NumberLock', 'CapsLock', 'TreatControlCAsInput', 'IsReadOnly', 'IsFixedSize', 'IsSynchronized'];
-	this.returnTypeDict['string'] = ['Join', 'Format', 'Copy', 'Concat', 'Intern', 'IsInterned', 'ReadLine', 'ToString', 'Substring', 'Trim', 'TrimStart', 'TrimEnd', 'Normalize', 'PadLeft', 'PadRight', 'ToLower', 'ToLowerInvariant', 'ToUpper', 'ToUpperInvariant', 'Insert', 'Replace', 'Remove', 'Title', 'Empty', 'TrueString', 'FalseString'];
+	this.returnTypeDict['string'] = ['Join', 'Format', 'Copy', 'Concat', 'Intern', 'IsInterned', 'ReadLine', 'ToString', 'Split', 'Substring', 'Trim', 'TrimStart', 'TrimEnd', 'Normalize', 'PadLeft', 'PadRight', 'ToLower', 'ToLowerInvariant', 'ToUpper', 'ToUpperInvariant', 'Insert', 'Replace', 'Remove', 'Title', 'Empty', 'TrueString', 'FalseString'];
 	this.returnTypeDict['double'] = ['Parse', 'Acos', 'Asin', 'Atan', 'Atan2', 'Ceiling', 'Cos', 'Cosh', 'Floor', 'Sin', 'Tan', 'Sinh', 'Tanh', 'Round', 'Truncate', 'Sqrt', 'Log', 'Log10', 'Exp', 'Pow', 'IEEERemainder', 'Abs', 'Max', 'Min', 'Sum', 'Average', 'MinValue', 'MaxValue', 'Epsilon', 'NegativeInfinity', 'PositiveInfinity', 'NaN', 'PI', 'E'];
 	this.returnTypeDict['Void'] = ['Beep', 'Clear', 'ResetColor', 'MoveBufferArea', 'SetBufferSize', 'SetWindowSize', 'SetWindowPosition', 'SetCursorPosition', 'SetIn', 'SetOut', 'SetError', 'WriteLine', 'Write', 'Resize', 'Copy', 'ConstrainedCopy', 'ForEach', 'Reverse', 'Sort', 'CopyTo', 'SetValue', 'Initialize', 'Add', 'AddRange', 'Insert', 'InsertRange', 'RemoveAt', 'RemoveRange', 'TrimExcess'];
 	this.returnTypeDict['ConsoleKeyInfo'] = ['ReadKey'];
 	this.returnTypeDict['IO.Stream'] = ['OpenStandardError', 'OpenStandardInput', 'OpenStandardOutput'];
+	this.returnTypeDict['Decimal'] = ['Ceiling', 'Floor', 'Round', 'Truncate', 'Abs', 'Max', 'Min', 'Sum', 'Average'];
 	this.returnTypeDict['SByte'] = ['Abs', 'Max', 'Min'];
 	this.returnTypeDict['long'] = ['Abs', 'Max', 'Min', 'BigMul', 'DivRem', 'Parse', 'LongCount', 'Sum', 'GetLongLength', 'LongLength', 'MaxValue', 'MinValue'];
 	this.returnTypeDict['Byte'] = ['Max', 'Min'];
-	this.returnTypeDict['Enumerable'] = ['Sum', 'Min', 'Max', 'Average', 'Except', 'Reverse', 'AsEnumerable', 'ToArray', 'ToList', 'ToDictionary', 'ToLookup', 'DefaultIfEmpty', 'OfType', 'Cast', 'Range', 'Repeat', 'Empty', 'Where', 'Select', 'SelectMany', 'Take', 'TakeWhile', 'Skip', 'SkipWhile', 'Join', 'GroupJoin', 'OrderBy', 'OrderByDescending', 'ThenBy', 'ThenByDescending', 'GroupBy', 'Concat', 'Zip', 'Distinct', 'Union', 'Intersect', 'AsReadOnly', 'ConvertAll', 'FindAll', 'ToCharArray', 'Split', 'GetEnumerator', 'GetRange'];
+	this.returnTypeDict['Enumerable'] = ['Sum', 'Min', 'Max', 'Average', 'Except', 'Reverse', 'AsEnumerable', 'ToArray', 'ToDictionary', 'ToLookup', 'DefaultIfEmpty', 'OfType', 'Cast', 'Range', 'Repeat', 'Empty', 'Where', 'Select', 'SelectMany', 'Take', 'TakeWhile', 'Skip', 'SkipWhile', 'Join', 'GroupJoin', 'OrderBy', 'OrderByDescending', 'ThenBy', 'ThenByDescending', 'GroupBy', 'Concat', 'Zip', 'Distinct', 'Union', 'Intersect', 'AsReadOnly', 'ConvertAll', 'FindAll', 'ToCharArray'];
 	this.returnTypeDict['TSource'] = ['Min', 'Max', 'First', 'FirstOrDefault', 'Last', 'LastOrDefault', 'Single', 'SingleOrDefault', 'ElementAt', 'ElementAtOrDefault', 'Aggregate'];
 	this.returnTypeDict['TResult'] = ['Min', 'Max', 'Aggregate'];
+	this.returnTypeDict['List'] = ['ToList', 'ConvertAll', 'FindAll', 'GetEnumerator', 'GetRange'];
 	this.returnTypeDict['TAccumulate'] = ['Aggregate'];
 	this.returnTypeDict['Array'] = ['CreateInstance'];
 	this.returnTypeDict['T'] = ['Find', 'FindLast', 'Item'];
@@ -87,9 +89,8 @@
 			if (beforeDot != undefined) {
 				res = this.AutocompliteAfterDotWords(beforeDot, start);
 			} else {
-				res = new SamePrefixArray(start);
+				res = new SamePrefixArray('');
 				res.AddAll(this.types);
-				res.AddAll(this.methods);
 				res.AddAll(this.keywords);
 			}
 		}
@@ -171,17 +172,18 @@ function arrayContains(arr, item) {
 }
 
 function SamePrefixArray(prefix) {
-	this.prefix = prefix.toLowerCase();
+	this.prefix = typeof this.prefix == "undefined" ? '' : prefix.toLowerCase();
 	this.container = [];
+
 	this.AddAll = function(list) {
 		for (var i = 0; i < list.length; ++i)
 			this.Add(list[i]);
 	}
-	this.Add = function (str) {
+	this.Add = function(str) {
 		if (str.toLowerCase().lastIndexOf(this.prefix, 0) == 0 && !arrayContains(this.container, str))
 			this.container.push(str);
 	}
-	this.getArray = function () {
+	this.getArray = function() {
 		return this.container;
 	}
 }

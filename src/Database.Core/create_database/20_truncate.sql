635af1819f8720d4989ae583b560d9178f15f657
vorkulsky <vorkulsky@skbkontur.ru>
2021-03-17 16:28:08 +0500

    pgloader

---
+++ src/Database.Core/create_database/20_truncate.sql
@@ -0,0 +1,12 @@
+﻿CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS $$
+DECLARE
+    statements CURSOR FOR
+        SELECT tablename, schemaname FROM pg_tables
+        WHERE tableowner = username AND (schemaname = 'antiplagiarism' or schemaname = 'public');
+BEGIN
+    FOR stmt IN statements LOOP
+            EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.schemaname) || '.' || quote_ident(stmt.tablename) || ' CASCADE;';
+        END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+SELECT truncate_tables('ulearn');
--- src/Database.Core/migrate_database_to_core/01_add_migrations_history.sql
+++ src/Database.Core/migrate_database_to_core/01_add_migrations_history.sql
@@ -4,4 +4,4 @@ INSERT INTO public."__EFMigrationsHistory"
     ("MigrationId", "ProductVersion")
 VALUES
 ('20210225124814_InitPostgres', '5.0.3'),
-('20210225124135_InitPostgres', '5.0.3');
+('20210312120452_Init', '5.0.3');
